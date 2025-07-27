a20034029be7187ee32c21df3602a2905276e253
Nassim <nassim.elhaddad2004@gmail.com>
2025-07-27 23:41:07 +0200

    Implement authentication flow and enhance ResumeCard component with score display

---
+++ app/routes/auth.tsx
@@ -0,0 +1,55 @@
+import React, { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
+
+export const meta = () => ([ 
+    { title: "CVision | Authentification" },
+    { name: "description", content: "Page d'authentification pour CVision" }
+]);
+
+const Auth: () => React.JSX.Element = () => {
+    const { isLoading, auth } = usePuterStore();
+    const location = useLocation();
+    const next = location.search.split('next=')[1];
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (auth.isAuthenticated) {
+            navigate(next);
+        }
+    }, [auth.isAuthenticated, navigate, next]);
+
+    return (
+        <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center"> 
+        <div className="gradient-border shadow-lg">
+            <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
+                <div className="flex flex-col items-center gap-2 text-center">
+                    <h1> Bienvenue ! </h1>
+                    <h2>Veuillez vous connecter pour continuer</h2>
+                </div>
+                <div>
+                    {isLoading ? (
+                        <button className="auth-button animate-pulse">
+                            <p> Connexion... </p>
+                        </button>
+                    ) : (
+                        <>
+                        {auth.isAuthenticated ? (
+                            <button className="auth-button" onClick={auth.signOut} > 
+                                <p> Se déconnecter </p>
+                            </button>
+                        ) : (
+                            <button className="auth-button" onClick={auth.signIn}>
+                                <p> Se connecter </p>
+                            </button>
+                        )}
+                        </>
+                    )}
+                </div>
+            </section>
+        </div>
+        </main>
+    )
+}
+
+export default Auth;
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -11,22 +11,23 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover bg-top bg-no-repeat min-h-screen w-full pt-4">
     <Navbar />
 
     <section className="main-section">
-      <div className="page-heading">
+      <div className="page-heading py-16">
         <h1>Bienvenue chez CVision</h1>
         <h2>Suivez vos candidatures, découvrez comment votre CV est évalué, consultez vos envois et profitez des retours personnalisés de l'IA</h2>
       </div>
-    </section>
+    
 
     {resumes.length > 0 && (
-      <div className="resume-section">
+      <div className="resumes-section">
         {resumes.map((resume) => (
           <ResumeCard key={resume.id} resume={resume} />
         ))}
       </div>
     )}
+    </section>
   </main>
 }
